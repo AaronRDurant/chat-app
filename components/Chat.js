d677fafeeef0e3e7c4ba641f6cf0c375f7e98e78
Aaron Durant <aaron@aarondurant.com>
2021-06-22 21:32:26 -0400

    Component and dependency updates
    Still working on getting this app to function properly...

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import {
+	Alert,
 	View,
 	Platform,
 	KeyboardAvoidingView,
 	StyleSheet,
 	LogBox,
 } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
+import AsyncStorage from '@react-native-community/async-storage';
+import NetInfo from '@react-native-community/netinfo';
 
 // Google Firebase
 const firebase = require('firebase');
 require('firebase/firestore');
 
-export default class Chat extends React.Component {
+class Chat extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -23,57 +26,71 @@ export default class Chat extends React.Component {
 				name: '',
 				avatar: '',
 			},
-		}
-
-		// Connects to Firebase database
-		const firebaseConfig = {
-			apiKey: "AIzaSyC3HZvZ9sQTkSMkN2MSBsDRn2EVQ2H1CkA",
-			authDomain: "chat-app-82d5d.firebaseapp.com",
-			projectId: "chat-app-82d5d",
-			storageBucket: "chat-app-82d5d.appspot.com",
-			messagingSenderId: "921296711417",
-			appId: "1:921296711417:web:ebf51852b118d02078bc28"
+			isConnected: false,
 		};
 
+		const firebaseConfig = {
+			apiKey: 'AIzaSyAr7k41KHO2yRylrf5P4TXlmIBYGmy1tlk',
+			authDomain: 'test-97f37.firebaseapp.com',
+			projectId: 'test-97f37',
+			storageBucket: 'test-97f37.appspot.com',
+			messagingSenderId: '883540949846',
+			appId: '1:883540949846:web:07e08bd6a925e0db66f4f7',
+			measurementId: 'G-23KWSF5HTT',
+		};
+
+		// Connect to Firebase
 		if (!firebase.apps.length) {
 			firebase.initializeApp(firebaseConfig);
 		}
 
+		// References Firebase messages
 		this.referenceChatMessages = firebase.firestore().collection('messages');
 		LogBox.ignoreLogs(['Setting a timer']);
 	}
 
-	// Sets state with a static message
 	componentDidMount() {
 		let { name } = this.props.route.params;
 
-		this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
-			if (!user) {
-				try {
-					await firebase.auth().signInAnonymously();
-			} catch (error) {
-				console.error(error.message);
-			}
-		}		
+		// Check user connection
+		NetInfo.fetch().then((connection) => {
+			if (connection.isConnected) {
+				this.setState({ isConnected: true });
 
-			// Updates user state
-			this.setState({
-				uid: user.uid,
-				user: {
-					_id: user.uid,
-					name: name,
-					avatar: 'https://placeimg.com/140/140/any',
-				},
-				messages: [],
-			});
-			
-			// Creates reference to active user's messages so user can see all messages
-			this.referenceChatMessages = firebase.firestore().collection('messages');
-			
-			// Listens for collection changes for current user
-			this.unsubscribeChatUser = this.referenceChatMessages
-				.orderBy('createdAt', 'desc')
-				.onSnapshot(this.onCollectionUpdate);
+				// Reference to load messages via Firebase
+				this.referenceChatMessages = firebase
+					.firestore()
+					.collection('messages');
+
+				// Authenticates user via Firebase
+				this.authUnsubscribe = firebase
+					.auth()
+					.onAuthStateChanged(async (user) => {
+						if (!user) {
+							await firebase.auth().signInAnonymously();
+						}
+						// Add user to state
+						this.setState({
+							uid: user.uid,
+							user: {
+								_id: user.uid,
+								name: name,
+								avatar: 'https://placeimg.com/140/140/any',
+							},
+							messages: [],
+						});
+						// Listener for collection changes for current user
+						this.unsubscribeChatUser = this.referenceChatMessages
+							.orderBy('createdAt', 'desc')
+							.onSnapshot(this.onCollectionUpdate);
+					});
+			} else {
+				this.setState({ isConnected: false });
+				this.getMessages();
+				Alert.alert(
+					'No internet connection detected | Unable to send messages'
+				);
+			}
 		});
 	}
 
@@ -87,7 +104,7 @@ export default class Chat extends React.Component {
 	// Updates messages state
 	onCollectionUpdate = (querySnapshot) => {
 		const messages = [];
-		// Iterates through each document
+		// Iterate through each document
 		querySnapshot.forEach((doc) => {
 			let data = doc.data(); // Grabs QueryDocumentSnapshot's data
 			messages.push({
@@ -104,7 +121,39 @@ export default class Chat extends React.Component {
 		this.setState({ messages });
 	};
 
-	// Adds messages to Firebase database
+	// Retrieve messages from client-side storage
+	getMessages = async () => {
+		let messages = '';
+		try {
+			messages = (await AsyncStorage.getItem('messages')) || [];
+			this.setState({ messages: JSON.parse(messages) });
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	// Saves messages in client-side storage
+	saveMessages = async () => {
+		try {
+			await AsyncStorage.setItem(
+				'messages',
+				JSON.stringify(this.state.messages)
+			);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	// Delete messages in client-side storage
+	deleteMessages = async () => {
+		try {
+			await AsyncStorage.removeItem('messages');
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	// Adds messages to cloud storage
 	addMessage() {
 		const message = this.state.messages[0];
 		this.referenceChatMessages.add({
@@ -116,7 +165,7 @@ export default class Chat extends React.Component {
 		});
 	}
 
-	// Event handler for when chat message is sent
+	// Event handler for sending messages
 	onSend(messages = []) {
 		this.setState(
 			(previousState) => ({
@@ -124,17 +173,27 @@ export default class Chat extends React.Component {
 			}),
 			() => {
 				this.addMessage();
-			});
+				this.saveMessages();
+			}
+		);
 	}
 
-	// Chat bubble customization
+	// Renders message input only when app is online
+	renderInputToolbar(props) {
+		if (this.state.isConnected == false) {
+		} else {
+			return <InputToolbar {...props} />;
+		}
+	}
+
+	// Renders sender's chat bubble with custom color
 	renderBubble(props) {
 		return (
 			<Bubble
 				{...props}
 				wrapperStyle={{
 					right: {
-						backgroundColor: '#1982FC',
+						backgroundColor: '#08d9d6',
 					},
 				}}
 			/>
@@ -142,7 +201,9 @@ export default class Chat extends React.Component {
 	}
 
 	render() {
-		const color = this.props.route.params.color; // Color selected on start page
+		const { name, color } = this.props.route.params;
+		const { messages } = this.state;
+
 		const styles = StyleSheet.create({
 			container: {
 				backgroundColor: color,
@@ -150,13 +211,12 @@ export default class Chat extends React.Component {
 			},
 		});
 
-		const { name } = this.props.route.params;
-		const { messages } = this.state;
-
 		return (
 			<View style={styles.container}>
 				<GiftedChat
 					renderBubble={this.renderBubble.bind(this)}
+					renderInputToolbar={this.renderInputToolbar.bind(this)}
+					renderUsernameOnMessage={true}
 					messages={messages}
 					onSend={(messages) => this.onSend(messages)}
 					user={{
@@ -165,6 +225,7 @@ export default class Chat extends React.Component {
 						name: name,
 					}}
 				/>
+				{/* Android keyboard fix */}
 				{Platform.OS === 'android' ? (
 					<KeyboardAvoidingView behavior='height' />
 				) : null}
@@ -172,3 +233,5 @@ export default class Chat extends React.Component {
 		);
 	}
 }
+
+export default Chat;
--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+	Alert,
 	StyleSheet,
 	View,
 	Text,
@@ -7,221 +8,238 @@ import {
 	TouchableOpacity,
 	KeyboardAvoidingView,
 	Platform,
-	Alert
 } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
-const backgroundImage = require('../assets/background-image.png');
-const keyboardVerticalOffset = Platform.OS === 'ios' ? 50 : 'height';
-
 const Start = ({ navigation }) => {
+	// Component State
 	const [name, setName] = useState('');
-	const [color, setColor] = useState('#ffffff');
-	const onButtonPress = (name) => {
-		if (name === "") {
-			// Alerts user to enter name if nothing was entered
-			return Alert.alert("Enter your name to continue!");
+	const [color, setColor] = useState('#090C08');
+
+	const keyboardVerticalOffset = Platform.OS === 'ios' ? 50 : 'height';
+	const background = require('../assets/background-image.png');
+
+	// Event Handler (when user clicks 'Start Chatting')
+	const handlePress = (text, color) => {
+		if (!text) {
+			return Alert.alert('Please enter a name');
 		}
 		navigation.navigate('Chat', { name, color });
-	}
-	
+	};
+
 	return (
-			<ImageBackground
-				style={styles.backgroundImage}
-				resizeMode='cover'
-				source={backgroundImage}
-			>
+		<ImageBackground
+			style={styles.background}
+			resizeMode='cover'
+			source={background}
+		>
 			<KeyboardAvoidingView
 				behavior='height'
 				keyboardVerticalOffset={keyboardVerticalOffset}
 				style={styles.container}
 				title='Chat App'
 			>
-			<Text style={styles.title}>Welcome to the Chat App</Text>
-			<View style={styles.box}>
-				<View style={styles.inputContainer}>
-					<TextInput
-						style={styles.textInput}
-						value={name}
-						onChangeText={(name) => setName(name)}
-						placeholder='Your name'
-					/>
+				<Text style={styles.title}>Welcome to the Chat App</Text>
+				<View style={styles.box}>
+					<View style={styles.inputContainer}>
+						<TextInput
+							style={styles.textInput}
+							value={name}
+							onChangeText={(name) => setName(name)}
+							placeholder='Your Name'
+						/>
+					</View>
+
+					<Text style={styles.text}>Choose Background Color:</Text>
+					<View style={styles.bgColorContainer}>
+						<TouchableOpacity
+							accessible={true}
+							accessibilityLabel='Black background'
+							accessibilityHint='Choose background color'
+							accessibilityRole='button'
+							style={styles.color1}
+							onPress={() => setColor('#090C08')}
+						></TouchableOpacity>
+						<TouchableOpacity
+							accessible={true}
+							accessibilityLabel='Purple background'
+							accessibilityHint='Choose background color'
+							accessibilityRole='button'
+							style={styles.color2}
+							onPress={() => setColor('#474056')}
+						></TouchableOpacity>
+						<TouchableOpacity
+							accessible={true}
+							accessibilityLabel='Blue background'
+							accessibilityHint='Choose background color'
+							accessibilityRole='button'
+							style={styles.color3}
+							onPress={() => setColor('#8095A5')}
+						></TouchableOpacity>
+						<TouchableOpacity
+							accessible={true}
+							accessibilityLabel='Green background'
+							accessibilityHint='Choose background color'
+							accessibilityRole='button'
+							style={styles.color4}
+							onPress={() => setColor('#B9C6AE')}
+						></TouchableOpacity>
+					</View>
+					<Text style={styles.selectedColor}>Selected Color:</Text>
+					<View
+						style={{
+							backgroundColor: color,
+							borderWidth: 1,
+							borderColor: '#2A323C',
+							borderRadius: 5,
+							width: '25%',
+							height: 20,
+							marginBottom: 10,
+							marginLeft: 20,
+						}}
+					></View>
+					<TouchableOpacity
+						accessible={true}
+						accessibilityLabel='Chat button'
+						accessibilityHint='Navigate to chat screen'
+						accessibilityRole='button'
+						style={styles.btnContainer}
+					>
+						<Text
+							style={styles.btnChat}
+							onPress={() => handlePress(name, color)}
+						>
+							Start Chatting
+						</Text>
+					</TouchableOpacity>
 				</View>
-				
-				<Text style={styles.text}>Choose background color:</Text>
-				<View style={styles.bgColorContainer}>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='Black background'
-						accessibilityHint='Choose background color'
-						accessibilityRole='button'
-						style={styles.color1}
-						onPress={() => setColor('#090C08')}
-					></TouchableOpacity>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='Purple background'
-						accessibilityHint='Choose background color'
-						accessibilityRole='button'
-						style={styles.color2}
-						onPress={() => setColor('#474056')}
-					></TouchableOpacity>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='Blue background'
-						accessibilityHint='Choose background color'
-						accessibilityRole='button'
-						style={styles.color3}
-						onPress={() => setColor('#8095A5')}
-					></TouchableOpacity>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='Green background'
-						accessibilityHint='Choose background color'
-						accessibilityRole='button'
-						style={styles.color4}
-						onPress={() => setColor('#B9C6AE')}
-					></TouchableOpacity>
-				</View>
-				<TouchableOpacity
-				accessible={true}
-				accessibilityLabel='Chat button'
-				accessibilityHint='Navigate to chat screen'
-				accessibilityRole='button'
-				style={styles.btnContainer}
-			>
-				<Text
-					style={styles.btnChat}
-					onPress={() => onButtonPress(name, color)}
-				>
-				Start Chatting
-				</Text>
-			</TouchableOpacity>
-			</View>
 			</KeyboardAvoidingView>
-			</ImageBackground>
-		);
-	}
+		</ImageBackground>
+	);
+};
 
 export default Start;
 
 // Styles
 const styles = StyleSheet.create({
 	container: {
-	flex: 1,
-	flexDirection: 'column',
-	alignItems: 'center',
+		flex: 1,
+		flexDirection: 'column',
+		alignItems: 'center',
+		// fontFamily: 'Poppins-Regular',
 	},
 	title: {
-	fontSize: 45,
-	fontWeight: '600',
-	color: '#FFF',
-	textAlign: 'center',
-	marginTop: 10,
+		fontSize: 45,
+		// fontFamily: 'Poppins-Bold',
+		fontWeight: '600',
+		color: '#FFF',
+		textAlign: 'center',
+		marginTop: 10,
 	},
-	backgroundImage: {
-	flex: 1,
+	background: {
+		flex: 1,
 	},
 	box: {
-	flex: 1,
-	borderWidth: 2,
-	borderColor: 'transparent',
-	borderRadius: 5,
-	margin: 20,
-	padding: 10,
-	width: '88%',
-	backgroundColor: '#fff',
-	textAlign: 'left',
-	position: 'absolute',
-	bottom: 10,
+		flex: 1,
+		borderWidth: 2,
+		borderColor: 'transparent',
+		borderRadius: 5,
+		margin: 20,
+		padding: 10,
+		width: '88%',
+		backgroundColor: '#fff',
+		textAlign: 'left',
+		position: 'absolute',
+		bottom: 10,
 	},
 	text: {
-	fontSize: 16,
-	fontWeight: '300',
-	color: '#757083',
-	opacity: 100,
-	marginTop: 20,
-	marginBottom: 10,
-	marginLeft: 20,
+		fontSize: 16,
+		fontWeight: '300',
+		color: '#757083',
+		opacity: 100,
+		marginTop: 20,
+		marginBottom: 10,
+		marginLeft: 20,
 	},
 	selectedColor: {
-	marginBottom: 10,
-	marginLeft: 20,
-	color: '#757083',
-	fontWeight: '300',
+		marginBottom: 10,
+		marginLeft: 20,
+		color: '#757083',
+		fontWeight: '300',
 	},
 	textInput: {
-	height: 50,
-	width: '90%',
-	borderColor: '#2A323C',
-	borderWidth: 1,
-	padding: 10,
-	fontSize: 16,
-	fontWeight: '300',
-	color: '#757083',
-	opacity: 50,
-	marginLeft: 'auto',
-	marginRight: 'auto',
-	marginTop: 10,
+		height: 50,
+		width: '90%',
+		borderColor: '#2A323C',
+		borderWidth: 1,
+		padding: 10,
+		fontSize: 16,
+		fontWeight: '300',
+		color: '#757083',
+		opacity: 50,
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginTop: 10,
 	},
 	inputContainer: {
-	flex: 0.3,
-	width: '100%',
-	marginBottom: 10,
+		flex: 0.3,
+		width: '100%',
+		marginBottom: 10,
 	},
 	bgColorContainer: {
-	flex: 0.5,
-	flexDirection: 'row',
-	marginBottom: 20,
-	marginLeft: 20,
+		flex: 0.5,
+		flexDirection: 'row',
+		marginBottom: 20,
+		marginLeft: 20,
 	},
 	color1: {
-	width: 40,
-	height: 40,
-	borderRadius: 25,
-	backgroundColor: '#090C08',
-	marginRight: 10,
-	marginTop: 10,
+		width: 40,
+		height: 40,
+		borderRadius: 25,
+		backgroundColor: '#090C08',
+		marginRight: 10,
+		marginTop: 10,
 	},
 	color2: {
-	width: 40,
-	height: 40,
-	borderRadius: 25,
-	backgroundColor: '#474056',
-	marginRight: 10,
-	marginTop: 10,
+		width: 40,
+		height: 40,
+		borderRadius: 25,
+		backgroundColor: '#474056',
+		marginRight: 10,
+		marginTop: 10,
 	},
 	color3: {
-	width: 40,
-	height: 40,
-	borderRadius: 25,
-	backgroundColor: '#8A95A5',
-	marginRight: 10,
-	marginTop: 10,
+		width: 40,
+		height: 40,
+		borderRadius: 25,
+		backgroundColor: '#8A95A5',
+		marginRight: 10,
+		marginTop: 10,
 	},
 	color4: {
-	width: 40,
-	height: 40,
-	borderRadius: 25,
-	backgroundColor: '#B9C6AE',
-	marginRight: 10,
-	marginTop: 10,
+		width: 40,
+		height: 40,
+		borderRadius: 25,
+		backgroundColor: '#B9C6AE',
+		marginRight: 10,
+		marginTop: 10,
 	},
 	btnChat: {
-	fontSize: 16,
-	fontWeight: '600',
-	color: '#fff',
-	padding: 20,
-	textAlign: 'center',
+		fontSize: 16,
+		// fontFamily: 'Poppins-Bold',
+		fontWeight: '600',
+		color: '#fff',
+		padding: 20,
+		textAlign: 'center',
 	},
 	btnContainer: {
-	backgroundColor: '#757083',
-	width: '88%',
-	borderWidth: 2,
-	borderColor: 'transparent',
-	borderRadius: 1,
-	marginRight: 'auto',
-	marginLeft: 'auto',
-	marginBottom: 10,
+		backgroundColor: '#757083',
+		width: '88%',
+		borderWidth: 2,
+		borderColor: 'transparent',
+		borderRadius: 1,
+		marginRight: 'auto',
+		marginLeft: 'auto',
+		marginBottom: 10,
 	},
 });
